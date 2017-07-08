518f38d61d58d696a37c8e2abd250fef581619e3
Bailey Stoner <monokrome@monokro.me>
2017-07-08 15:30:11 -0700

    (split) Don't make any unnecessary OpenGL calls.

--- src/video_opengl.c
+++ src/video_opengl.c
@@ -38,29 +38,12 @@ void load(HAPEngine *engine, void *state, char *identifier) {
 	(void)engine;      // Mark variable as used to avoid compiler warnings
 	(void)state;       // Mark variable as used to avoid compiler warnings
 	(void)identifier;  // Mark variable as used to avoid compiler warnings
-
-	glMatrixMode(GL_PROJECTION);
-	glLoadIdentity();
-	glOrtho(-1., 1., -1., 1., 1., 20.);
-
-	glMatrixMode(GL_MODELVIEW);
-	glLoadIdentity();
-	gluLookAt(0., 0., 10., 0., 0., 0., 0., 1., 0.);
-
-	glClearColor(1.0, 1.0, 1.0, 1.0);
-	glEnable(GL_DEPTH_TEST);
 }
 
 HAPTime update(HAPEngine *engine, void *state) {
 	if (window_update(state) < 0) return -1;
 
-	float color = (sin((*(*engine).time).currentTime) / 2) + .5f;
-
-	glClearColor((color / 2) * .7f, color * .3f, color * .7f, 1.0);
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
 	window_render(state);
-
 	return 0;
 }
 
