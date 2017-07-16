ab9a795cb8d5dd5d56708213d1ffffd5fe81d816
Bailey Stoner <monokrome@monokro.me>
2017-07-16 16:26:52 -0700

    (split) Add support for windowing in OSX
    This is a "fallback" driver using SDL.

--- src/fallback.c
+++ src/fallback.c
@@ -9,19 +9,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include <SDL/SDL.h>
+#include <SDL2/SDL.h>
 
 #include "window_management.h"
 
 
-struct SDLWindow {
-  SDL_Surface *screen;
+struct FallbackWindow {
+  SDL_Window *ref;
 };
 
-typedef struct SDLWindow SDLWindow;
+typedef struct FallbackWindow FallbackWindow;
+
+int is_window_event(void* userData, SDL_Event *event) {
+  return (*event).type == SDL_WINDOWEVENT;
+}
 
 void* window_create(HAPEngine *engine, const int width, const int height) {
-  SDLWindow *window;
+  FallbackWindow *window;
 
   if (SDL_Init(SDL_INIT_VIDEO) < 0) {
     fprintf(stderr, "Could not initialize SDL video.\n");
@@ -30,23 +34,47 @@ void* window_create(HAPEngine *engine, const int width, const int height) {
 
   atexit(SDL_Quit);
 
-  window = (SDLWindow*) calloc(1, sizeof(SDLWindow));
+  window = (FallbackWindow*) calloc(1, sizeof(FallbackWindow));
   if (window == NULL) {
     fprintf(stderr, "Failed to .\n");
     return NULL;
   }
 
-  (*window).screen = SDL_SetVideoMode(width, height, 8, SDL_SWSURFACE);
+  (*window).ref = SDL_CreateWindow(
+	(*engine).name,
+	SDL_WINDOWPOS_UNDEFINED,
+	SDL_WINDOWPOS_UNDEFINED,
+	width,
+	height,
+	SDL_WINDOW_OPENGL
+  );
 
-  if ((*window).screen == NULL) {
-    fprintf(stderr, "Failed to set video mode.\n");
+  if ((*window).ref == NULL) {
+    fprintf(stderr, "Failed to create SDL window.");
     return NULL;
   }
+
+  SDL_ShowWindow((*window).ref);
 
   return (void*) window;
 }
 
 int window_update(void* state) {
+  SDL_Event event;
+
+  // We only care about window events in this driver
+  SDL_SetEventFilter(is_window_event, NULL);
+
+  while (SDL_PollEvent(&event)) {
+    switch (event.type) {
+      case SDL_WINDOWEVENT:
+	if (event.window.event == SDL_WINDOWEVENT_CLOSE) return -1;
+    }
+  }
+
+  // Clear the event filter so that we don't interfere with other modules
+  SDL_SetEventFilter(NULL, NULL);
+
   return 0;
 }
 
@@ -54,8 +82,11 @@ void window_render(void* state) {
 }
 
 void window_close(void* state) {
-  SDLWindow *window = (SDLWindow*) state;
+  FallbackWindow *window = (FallbackWindow*) state;
+  if ((*window).ref != NULL) SDL_DestroyWindow((*window).ref);
   free(state);
+
+  SDL_Quit();
 }
 
 
