1f180c4ecf20e1b519db277e7001415128b125d9
Bailey Stoner <bailey@patreon.com>
2017-07-20 19:37:06 -0700

    (split) Fix formatting of fallback driver.

--- src/fallback.c
+++ src/fallback.c
@@ -15,78 +15,78 @@
 
 
 struct FallbackWindow {
-  SDL_Window *ref;
+    SDL_Window *ref;
 };
 
 typedef struct FallbackWindow FallbackWindow;
 
 int is_window_event(void* userData, SDL_Event *event) {
-  return (*event).type == SDL_WINDOWEVENT;
+    return (*event).type == SDL_WINDOWEVENT;
 }
 
 void* window_create(HAPEngine *engine, const int width, const int height) {
-  FallbackWindow *window;
+    FallbackWindow *window;
 
-  if (SDL_Init(SDL_INIT_VIDEO) < 0) {
-    fprintf(stderr, "Could not initialize SDL video.\n");
-    return NULL;
-  }
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        fprintf(stderr, "Could not initialize SDL video.\n");
+        return NULL;
+    }
 
-  atexit(SDL_Quit);
+    atexit(SDL_Quit);
 
-  window = (FallbackWindow*) calloc(1, sizeof(FallbackWindow));
-  if (window == NULL) {
-    fprintf(stderr, "Failed to .\n");
-    return NULL;
-  }
+    window = (FallbackWindow*) calloc(1, sizeof(FallbackWindow));
+    if (window == NULL) {
+        fprintf(stderr, "Failed to .\n");
+        return NULL;
+    }
 
-  (*window).ref = SDL_CreateWindow(
-	(*engine).name,
-	SDL_WINDOWPOS_UNDEFINED,
-	SDL_WINDOWPOS_UNDEFINED,
-	width,
-	height,
-	SDL_WINDOW_OPENGL
-  );
+    (*window).ref = SDL_CreateWindow(
+                        (*engine).name,
+                        SDL_WINDOWPOS_UNDEFINED,
+                        SDL_WINDOWPOS_UNDEFINED,
+                        width,
+                        height,
+                        SDL_WINDOW_OPENGL
+                    );
 
-  if ((*window).ref == NULL) {
-    fprintf(stderr, "Failed to create SDL window.");
-    return NULL;
-  }
+    if ((*window).ref == NULL) {
+        fprintf(stderr, "Failed to create SDL window.");
+        return NULL;
+    }
 
-  SDL_ShowWindow((*window).ref);
+    SDL_ShowWindow((*window).ref);
 
-  return (void*) window;
+    return (void*) window;
 }
 
 int window_update(void* state) {
-  SDL_Event event;
+    SDL_Event event;
 
-  // We only care about window events in this driver
-  SDL_SetEventFilter(is_window_event, NULL);
+    // We only care about window events in this driver
+    SDL_SetEventFilter(is_window_event, NULL);
 
-  while (SDL_PollEvent(&event)) {
-    switch (event.type) {
-      case SDL_WINDOWEVENT:
-	if (event.window.event == SDL_WINDOWEVENT_CLOSE) return -1;
+    while (SDL_PollEvent(&event)) {
+        switch (event.type) {
+        case SDL_WINDOWEVENT:
+            if (event.window.event == SDL_WINDOWEVENT_CLOSE) return -1;
+        }
     }
-  }
 
-  // Clear the event filter so that we don't interfere with other modules
-  SDL_SetEventFilter(NULL, NULL);
+    // Clear the event filter so that we don't interfere with other modules
+    SDL_SetEventFilter(NULL, NULL);
 
-  return 0;
+    return 0;
 }
 
 void window_render(void* state) {
 }
 
 void window_close(void* state) {
-  FallbackWindow *window = (FallbackWindow*) state;
-  if ((*window).ref != NULL) SDL_DestroyWindow((*window).ref);
-  free(state);
+    FallbackWindow *window = (FallbackWindow*) state;
+    if ((*window).ref != NULL) SDL_DestroyWindow((*window).ref);
+    free(state);
 
-  SDL_Quit();
+    SDL_Quit();
 }
 
 
